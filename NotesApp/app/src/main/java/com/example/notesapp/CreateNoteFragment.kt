5e5c6c6f6a2fbb8c9fb8303ff8d58ac23822f12e
comye1 <50735594+comye1@users.noreply.github.com>
2021-01-26 20:01:08 +0900

    HomeFragment & CreateNoteFragment implemented

---
+++ NotesApp/app/src/main/java/com/example/notesapp/CreateNoteFragment.kt
@@ -0,0 +1,104 @@
+package com.example.notesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import com.example.notesapp.databinding.FragmentCreateNoteBinding
+import com.example.notesapp.databinding.FragmentHomeBinding
+import java.text.SimpleDateFormat
+import java.util.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [CreateNoteFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class CreateNoteFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    private var _binding: FragmentCreateNoteBinding? = null
+
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        _binding = FragmentCreateNoteBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+
+//        return inflater.inflate(R.layout.fragment_create_note, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
+        val currentDate = sdf.format(Date())
+
+        binding.tvDateTime.text = currentDate
+        binding.imgDone.setOnClickListener {
+            //save note
+            saveNote()
+        }
+
+        binding.imgBack.setOnClickListener {
+            replaceFragment(HomeFragment.newInstance(), false)
+        }
+    }
+
+    private fun saveNote(){
+
+        if(binding.etNoteTitle.text.isNullOrEmpty()){
+            Toast.makeText(context, "제목을 입력해주세요.", Toast.LENGTH_SHORT).show()
+        }
+        if(binding.etNoteSubTitle.text.isNullOrEmpty()){
+            Toast.makeText(context, "소제목을 입력해주세요.", Toast.LENGTH_SHORT).show()
+        }
+        if(binding.etNoteDesc.text.isNullOrEmpty()) {
+            Toast.makeText(context, "내용을 입력해주세요.", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun replaceFragment(fragment:Fragment, isTransition:Boolean){
+        val fragmentTransition = activity!!.supportFragmentManager.beginTransaction()
+
+        if (isTransition){
+            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
+        }
+        fragmentTransition.replace(R.id.frameLayout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance() =
+                CreateNoteFragment().apply {
+                    arguments = Bundle().apply {
+                    }
+                }
+    }
+}
--- NotesApp/app/src/main/java/com/example/notesapp/HomeFragment.kt
+++ NotesApp/app/src/main/java/com/example/notesapp/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.notesapp.databinding.FragmentHomeBinding
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,12 +22,18 @@ class HomeFragment : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+    private var _binding:FragmentHomeBinding? = null
+
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+        // This property is only valid between onCreateView and
+        // onDestroyView.
     }
 
     override fun onCreateView(
@@ -34,19 +41,25 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
+//        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.fabCreateNote.setOnClickListener {
+            replaceFragment(CreateNoteFragment.newInstance(), true);
+        }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance() =
             HomeFragment().apply {
@@ -54,4 +67,15 @@ class HomeFragment : Fragment() {
                 }
             }
     }
+
+
+    fun replaceFragment(fragment:Fragment, isTransition:Boolean){
+        val fragmentTransition = activity!!.supportFragmentManager.beginTransaction()
+
+        if (isTransition){
+            fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
+        }
+        fragmentTransition.replace(R.id.frameLayout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
+    }
+
 }
