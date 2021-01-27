ac72cfab51cd98572f92f5a8e702be967dfea28d
comye1 <50735594+comye1@users.noreply.github.com>
2021-01-27 22:35:39 +0900

    Fragment 전환 - commit 필수!!

--- NotesApp/app/src/main/java/com/example/notesapp/HomeFragment.kt
+++ NotesApp/app/src/main/java/com/example/notesapp/HomeFragment.kt
@@ -17,20 +17,13 @@ private const val ARG_PARAM2 = "param2"
  * Use the [HomeFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
+class HomeFragment : BaseFragment() {
     private var _binding:FragmentHomeBinding? = null
-
     private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
         }
         // This property is only valid between onCreateView and
         // onDestroyView.
@@ -49,8 +42,8 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.fabCreateNote.setOnClickListener {
-            replaceFragment(CreateNoteFragment.newInstance(), true);
+        binding.fabCreateNote.setOnClickListener{
+            replaceFragment(CreateNoteFragment.newInstance(), false)
         }
     }
 
@@ -75,7 +68,7 @@ class HomeFragment : Fragment() {
         if (isTransition){
             fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right,android.R.anim.slide_in_left)
         }
-        fragmentTransition.replace(R.id.frameLayout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
+        fragmentTransition.replace(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
     }
 
 }
--- NotesApp/app/src/main/java/com/example/notesapp/MainActivity.kt
+++ NotesApp/app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -2,17 +2,18 @@ package com.example.notesapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import com.example.notesapp.databinding.ActivityMainBinding
 import com.example.notesapp.databinding.FragmentHomeBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: FragmentHomeBinding
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = FragmentHomeBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         val view = binding.root
 //        setContentView(R.layout.activity_main)
         setContentView(view)
@@ -26,7 +27,7 @@ class MainActivity : AppCompatActivity() {
             fragmentTransition.setCustomAnimations(android.R.anim.slide_out_right, android.R.anim.slide_in_left)
         }
 
-        fragmentTransition.replace(R.id.frameLayout,fragment).addToBackStack(fragment.javaClass.simpleName)
-
+        fragmentTransition.replace(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName)
+        fragmentTransition.commit()
     }
 }
