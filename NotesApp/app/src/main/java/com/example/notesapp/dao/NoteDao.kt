6c85cf8b9919e643310b0a92ab976bd11d5d925d
comye1 <50735594+comye1@users.noreply.github.com>
2021-01-27 22:31:20 +0900

    Model / DAO 수정

--- NotesApp/app/src/main/java/com/example/notesapp/dao/NoteDao.kt
+++ NotesApp/app/src/main/java/com/example/notesapp/dao/NoteDao.kt
@@ -9,8 +9,8 @@ interface NoteDao {
     val allNotes:List<Notes>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun insertNotes(note:Notes)
+    suspend fun insertNotes(note:Notes)
 
     @Delete
-    fun deleteNote(note:Notes)
+    suspend fun deleteNote(note:Notes)
 }
