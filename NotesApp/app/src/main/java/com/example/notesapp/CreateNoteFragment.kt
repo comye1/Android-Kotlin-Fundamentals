7bf483ec350d9267dcf5701a5af5621eded563cf
comye1 <50735594+comye1@users.noreply.github.com>
2021-02-05 18:12:29 +0900

    CreateNoteFragment.kt - BroadCastReceiver

--- NotesApp/app/src/main/java/com/example/notesapp/CreateNoteFragment.kt
+++ NotesApp/app/src/main/java/com/example/notesapp/CreateNoteFragment.kt
@@ -1,15 +1,22 @@
 package com.example.notesapp
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.example.notesapp.database.NotesDatabase
 import com.example.notesapp.databinding.FragmentCreateNoteBinding
 import com.example.notesapp.databinding.FragmentHomeBinding
 import com.example.notesapp.entities.Notes
+import com.example.notesapp.util.NoteBottomSheetFragment
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
@@ -56,8 +63,15 @@ class CreateNoteFragment : BaseFragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        LocalBroadcastManager.getInstance(requireContext()).registerReceiver(
+                BroadcastReceiver, IntentFilter("bottom_sheet_action")
+        )
+
         val sdf = SimpleDateFormat("dd/M/yyyy hh:mm:ss")
         currentDate = sdf.format(Date())
+
+        binding.colorView.setBackgroundColor(Color.parseColor(selectedColor))/////////
 
         binding.tvDateTime.text = currentDate
         binding.imgDone.setOnClickListener {
@@ -67,6 +81,11 @@ class CreateNoteFragment : BaseFragment() {
 
         binding.imgBack.setOnClickListener {
             replaceFragment(HomeFragment.newInstance(), false)
+        }
+
+        binding.imgMore.setOnClickListener {
+            var noteBottomSheetFragment = NoteBottomSheetFragment.newInstance()
+            noteBottomSheetFragment.show(requireActivity().supportFragmentManager, "Note Bottom Sheet Fragment")
         }
     }
 
@@ -107,9 +126,61 @@ class CreateNoteFragment : BaseFragment() {
         fragmentTransition.replace(R.id.frame_layout,fragment).addToBackStack(fragment.javaClass.simpleName).commit()
     }
 
+    private val BroadcastReceiver : BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+
+            var actionColor = intent!!.getStringExtra("actionColor")
+            when(actionColor!!){
+                "Blue" -> {
+                    selectedColor = intent!!.getStringExtra("selectedColor")!!
+                    binding.colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+
+                "Yellow" -> {
+                    selectedColor = intent!!.getStringExtra("selectedColor")!!
+                    binding.colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+
+                "Purple" -> {
+                    selectedColor = intent!!.getStringExtra("selectedColor")!!
+                    binding.colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+
+                "Green" -> {
+                    selectedColor = intent!!.getStringExtra("selectedColor")!!
+                    binding.colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+
+                "Orange" -> {
+                    selectedColor = intent!!.getStringExtra("selectedColor")!!
+                    binding.colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+
+                "Black" -> {
+                    selectedColor = intent!!.getStringExtra("selectedColor")!!
+                    binding.colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+
+                else -> {
+                    selectedColor = intent!!.getStringExtra("selectedColor")!!
+                    binding.colorView.setBackgroundColor(Color.parseColor(selectedColor))
+                }
+            }
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    override fun onDestroy() {
+        LocalBroadcastManager.getInstance(requireContext()).unregisterReceiver(BroadcastReceiver)
+        super.onDestroy()
     }
 
     companion object {
