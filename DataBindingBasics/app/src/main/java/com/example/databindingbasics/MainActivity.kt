dfd8a28d1464a850145353c5b6a8a95f74959029
comye1 <50735594+comye1@users.noreply.github.com>
2021-02-15 01:10:09 +0900

    apply view binding

--- DataBindingBasics/app/src/main/java/com/example/databindingbasics/MainActivity.kt
+++ DataBindingBasics/app/src/main/java/com/example/databindingbasics/MainActivity.kt
@@ -9,25 +9,50 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.databindingbasics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding:ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//        setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.button).setOnClickListener {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+//        findViewById<Button>(R.id.button).setOnClickListener {
+//            setID(it)
+//        }
+        // (2)
+        binding.button.setOnClickListener {
             setID(it)
         }
     }
 
     private fun setID(view: View){
-        val editText = findViewById<EditText>(R.id.edit)
-        val textView = findViewById<TextView>(R.id.text)
+//        val editText = findViewById<EditText>(R.id.edit)
+//        val textView = findViewById<TextView>(R.id.text)
 
-        textView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        textView.visibility = View.VISIBLE
+//        textView.text = editText.text
+//        editText.visibility = View.GONE
+//        view.visibility = View.GONE
+//        textView.visibility = View.VISIBLE
+
+        //(3)
+//        binding.text.text = binding.edit.text
+//        binding.edit.visibility = View.GONE
+//        binding.button.visibility = View.GONE
+//        binding.text.visibility = View.VISIBLE
+
+        binding.apply {
+            text.text = edit.text
+            edit.visibility = View.GONE
+            button.visibility = View.GONE
+            text.visibility = View.VISIBLE
+        }
+
 
         // Hide the keyboard.
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
