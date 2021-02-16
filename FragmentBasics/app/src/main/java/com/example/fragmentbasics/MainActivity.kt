8f89b656d771a23c96012da4be0bdd9b168f309a
comye1 <50735594+comye1@users.noreply.github.com>
2021-02-16 15:34:13 +0900

    data binding 설정 / main activity implement

--- FragmentBasics/app/src/main/java/com/example/fragmentbasics/MainActivity.kt
+++ FragmentBasics/app/src/main/java/com/example/fragmentbasics/MainActivity.kt
@@ -2,10 +2,16 @@ package com.example.fragmentbasics
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.fragmentbasics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+//        setContentView(R.layout.activity_main)
+        @Suppress("UNUSED_VARIABLE")
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
+
 }
