b10115a03906022021987108cd7aa6f852e7b3ab
comye1 <50735594+comye1@users.noreply.github.com>
2021-02-17 22:12:11 +0900

    add fragment and implement layout

--- FragmentBasics/app/src/main/java/com/example/fragmentbasics/MainActivity.kt
+++ FragmentBasics/app/src/main/java/com/example/fragmentbasics/MainActivity.kt
@@ -10,7 +10,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
 //        setContentView(R.layout.activity_main)
-        @Suppress("UNUSED_VARIABLE")
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
     }
 
