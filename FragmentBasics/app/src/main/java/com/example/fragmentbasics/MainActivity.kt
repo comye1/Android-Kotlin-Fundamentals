07bc20add8611793d0169a12bba8719ec3a5b22d
comye1 <50735594+comye1@users.noreply.github.com>
2021-02-22 21:31:17 +0900

    up button

--- FragmentBasics/app/src/main/java/com/example/fragmentbasics/MainActivity.kt
+++ FragmentBasics/app/src/main/java/com/example/fragmentbasics/MainActivity.kt
@@ -3,6 +3,8 @@ package com.example.fragmentbasics
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.fragmentbasics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +13,13 @@ class MainActivity : AppCompatActivity() {
 
 //        setContentView(R.layout.activity_main)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        val navController = this.findNavController(R.id.navHostFragment)
+
+        NavigationUI.setupActionBarWithNavController(this, navController)
     }
 
+    override fun onSupportNavigateUp(): Boolean {
+        val navController = this.findNavController(R.id.navHostFragment)
+        return navController.navigateUp()
+    }
 }
