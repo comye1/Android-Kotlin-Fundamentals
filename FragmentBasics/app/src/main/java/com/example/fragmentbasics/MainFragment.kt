d2e29a48a0e68317053f53ba357330a8a264a031
comye1 <50735594+comye1@users.noreply.github.com>
2021-02-24 15:32:26 +0900

    add optionsMenu to MainFragment

--- FragmentBasics/app/src/main/java/com/example/fragmentbasics/MainFragment.kt
+++ FragmentBasics/app/src/main/java/com/example/fragmentbasics/MainFragment.kt
@@ -1,12 +1,12 @@
 package com.example.fragmentbasics
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import com.example.fragmentbasics.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -23,6 +23,22 @@ class MainFragment : Fragment() {
         binding.startBtn.setOnClickListener { view ->
             view.findNavController().navigate(R.id.action_mainFragment_to_pickFragment)
         }
+        setHasOptionsMenu(true)
         return binding.root
     }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.main_menu, menu) //추가
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+                || super.onOptionsItemSelected(item)
+    }
+
+
+//        NavigationUI.onNavDestinationSelected(item, requireView().findNavController())
+//        return super.onOptionsItemSelected(item)
+
 }
