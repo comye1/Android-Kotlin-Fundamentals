bb32fa5f74b4cb8cc70f6f8ec0acb94796db31d9
comye1 <50735594+comye1@users.noreply.github.com>
2021-02-27 15:33:15 +0900

    show drawer

--- FragmentBasics/app/src/main/java/com/example/fragmentbasics/MainActivity.kt
+++ FragmentBasics/app/src/main/java/com/example/fragmentbasics/MainActivity.kt
@@ -3,25 +3,32 @@ package com.example.fragmentbasics
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.fragmentbasics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var drawerLayout: DrawerLayout // null safety
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 //        setContentView(R.layout.activity_main)
         val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
         val navController = this.findNavController(R.id.navHostFragment)
+        drawerLayout = binding.drawerLayout // initialize
 
 //        NavigationUI.setupActionBarWithNavController(this, navController)
-        NavigationUI.setupActionBarWithNavController(this,navController, drawer)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(binding.navView, navController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         val navController = this.findNavController(R.id.navHostFragment)
-        return navController.navigateUp()
+//        return navController.navigateUp()
+        return NavigationUI.navigateUp(navController, drawerLayout)
     }
 }
