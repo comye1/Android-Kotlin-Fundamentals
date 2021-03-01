59fe632d17e18608ed0664440f467add8447c2e5
comye1 <50735594+comye1@users.noreply.github.com>
2021-03-01 12:25:29 +0900

    Add data binding for the GameViewModel

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameFragment.kt
@@ -65,6 +65,9 @@ class GameFragment : Fragment() {
             if(hasFinished) gameFinished()
         })
 
+        // Set the ViewModel for databinding - this allows the bound layout access
+        // to all the data in the ViewModel
+        binding.gameViewModel = viewModel
         binding.correctButton.setOnClickListener { onCorrect() }
         binding.skipButton.setOnClickListener { onSkip() }
         binding.endGameButton.setOnClickListener { onEndGame() }
