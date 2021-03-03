2ffa937c765fffaef8550d6b6a2789720899daf3
comye1 <50735594+comye1@users.noreply.github.com>
2021-03-03 14:03:45 +0900

    Share Text

--- SharingFeature/app/src/main/java/com/example/sharingfeature/MainActivity.kt
+++ SharingFeature/app/src/main/java/com/example/sharingfeature/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.example.sharingfeature
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.sharingfeature.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        binding.buttonShare.setOnClickListener {
+            shareText(binding.editText.text.toString())
+        }
+    }
+
+    private fun shareText(text:String){
+        startActivity(getShareIntent(text))
+    }
+
+    private fun getShareIntent(text: String):Intent{
+        val shareIntent = Intent(Intent.ACTION_SEND)
+            .setType("text/plain")
+            .putExtra(Intent.EXTRA_TEXT, text)
+
+        return shareIntent
     }
 }
