84777c90ba3a1305425021dc2bd4d92ac0d034bf
comye1 <50735594+comye1@users.noreply.github.com>
2021-03-05 21:48:33 +0900

    add dataBinding and navigation code

--- SafeArgs/app/src/main/java/com/example/safeargs/screens/StartFragment.kt
+++ SafeArgs/app/src/main/java/com/example/safeargs/screens/StartFragment.kt
@@ -5,14 +5,40 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import com.example.safeargs.R
+import com.example.safeargs.databinding.FragmentStartBinding
+
 class StartFragment : Fragment() {
+    
+    private lateinit var binding : FragmentStartBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_start,
+            container,
+            false
+        )
+
+        binding.button.setOnClickListener {view ->
+            if(checkName()){
+                view.findNavController()
+                    .navigate(PicFragmentDirections.actionPicFragmentToResultFragment())
+            }
+        }
+
+        return binding.root
+    }
+
+
+    private fun checkName() : Boolean{
+        //이름이 빈 문자열인지 확인하기
+        return binding.editText.text.isNullOrBlank()
     }
 }
