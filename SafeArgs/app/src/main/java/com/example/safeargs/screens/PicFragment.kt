4a6912f56ca3806260b1dd785214b39b1445198b
comye1 <50735594+comye1@users.noreply.github.com>
2021-03-05 23:06:27 +0900

    implement PicFragment & use Args

--- SafeArgs/app/src/main/java/com/example/safeargs/screens/PicFragment.kt
+++ SafeArgs/app/src/main/java/com/example/safeargs/screens/PicFragment.kt
@@ -5,15 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.navArgs
 import com.example.safeargs.R
+import com.example.safeargs.databinding.FragmentPicBinding
 
 class PicFragment : Fragment() {
+
+    private lateinit var binding : FragmentPicBinding
+
+    // get Args
+    val args: PicFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pic, container, false)
+        binding = DataBindingUtil.inflate(
+            inflater,
+            R.layout.fragment_pic,
+            container,
+            false
+        )
+        //set textView with petName from args
+        binding.petNameTextView.text = args.petName
+
+        return binding.root
     }
 }
