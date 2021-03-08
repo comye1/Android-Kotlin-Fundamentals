a7302cacf6065ba7f61fed0d0869841b3c120803
comye1 <50735594+comye1@users.noreply.github.com>
2021-03-09 01:02:07 +0900

    add feature to pick an image from gallery and set it to imageView

--- SafeArgs/app/src/main/java/com/example/safeargs/screens/PicFragment.kt
+++ SafeArgs/app/src/main/java/com/example/safeargs/screens/PicFragment.kt
@@ -1,10 +1,13 @@
 package com.example.safeargs.screens
 
+import android.app.Activity.RESULT_OK
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.navArgs
 import com.example.safeargs.R
@@ -31,6 +34,31 @@ class PicFragment : Fragment() {
         //set textView with petName from args
         binding.petNameTextView.text = args.petName
 
+        binding.buttonSelect.setOnClickListener {
+            pickImageGallery()
+        }
+        binding.buttonComplete.setOnClickListener {
+
+        }
+
         return binding.root
+    }
+
+    private fun pickImageGallery() {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_REQUEST_CODE)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == IMAGE_REQUEST_CODE && resultCode == RESULT_OK){
+            binding.imageView.setImageURI(data?.data)
+            binding.imageView.scaleType = ImageView.ScaleType.FIT_CENTER
+        }
+    }
+
+    companion object{
+        val IMAGE_REQUEST_CODE = 100
     }
 }
