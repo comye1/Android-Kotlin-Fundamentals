ade46ac51233ac1cd0b500e3a45d03115d08ea8e
comye1 <50735594+comye1@users.noreply.github.com>
2021-03-09 13:08:23 +0900

    implement resultFragment show pet's name and img passed by picFragment

--- SafeArgs/app/src/main/java/com/example/safeargs/screens/ResultFragment.kt
+++ SafeArgs/app/src/main/java/com/example/safeargs/screens/ResultFragment.kt
@@ -1,19 +1,33 @@
 package com.example.safeargs.screens
 
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.navArgs
 import com.example.safeargs.R
+import com.example.safeargs.databinding.FragmentResultBinding
+
 class ResultFragment : Fragment() {
+
+    lateinit var binding : FragmentResultBinding
+
+    val args : ResultFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_result, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_result, container, false)
+
+        binding.imagePet.setImageURI(Uri.parse(args.imgPath))
+        binding.textPetName.text = args.petName
+
+        return binding.root
     }
 
 
