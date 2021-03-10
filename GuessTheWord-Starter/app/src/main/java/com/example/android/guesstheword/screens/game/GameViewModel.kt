f5f8e2f8714dd9e5ca59447f4cabf066e1012c93
comye1 <50735594+comye1@users.noreply.github.com>
2021-03-10 15:48:45 +0900

    add word hint with LiveData Transformation

--- GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
+++ GuessTheWord-Starter/app/src/main/java/com/example/android/guesstheword/screens/game/GameViewModel.kt
@@ -18,6 +18,7 @@ class GameViewModel : ViewModel() {
         //total time - 60초
         private const val COUNTDOWN_TIME = 60000L
     }
+
     //Countdown time
     private val _currentTime = MutableLiveData<Long>()
     val currentTime: LiveData<Long>
@@ -34,6 +35,15 @@ class GameViewModel : ViewModel() {
     val word : LiveData<String>
         get() = _word
 
+
+    val wordHint = Transformations.map(word) { word->
+        val randomPosition = (1..word.length).random()
+        "Current word has " + word.length + " letters\n"+
+                "The letter at position " + randomPosition + " is " +
+                word.get(randomPosition - 1).toUpperCase()
+    }
+
+
     // The current score
     private val _score = MutableLiveData<Int>()
     val score : LiveData<Int>
@@ -47,6 +57,29 @@ class GameViewModel : ViewModel() {
 
     // The list of words - the front of the list is the next word to guess
     private lateinit var wordList: MutableList<String>
+
+    init {
+        //initialize LiveData, setValue()
+        _word.value = ""
+        _score.value = 0
+
+        resetList()
+        nextWord()
+
+        //initialize and start the timer
+        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND){
+            override fun onTick(millisUntilFinished: Long) {
+                _currentTime.value = millisUntilFinished/ ONE_SECOND
+            }
+
+            override fun onFinish() {
+                _currentTime.value = DONE
+                onGameFinish()
+            }
+        }
+        timer.start()
+    }
+
     /**
      * Resets the list of words and randomizes the order
      */
@@ -77,27 +110,6 @@ class GameViewModel : ViewModel() {
         wordList.shuffle()
     }
 
-    init {
-        //initialize LiveData, setValue()
-        _word.value = ""
-        _score.value = 0
-
-        resetList()
-        nextWord()
-
-        //initialize and start the timer
-        timer = object : CountDownTimer(COUNTDOWN_TIME, ONE_SECOND){
-            override fun onTick(millisUntilFinished: Long) {
-                _currentTime.value = millisUntilFinished/ ONE_SECOND
-            }
-
-            override fun onFinish() {
-                _currentTime.value = DONE
-                onGameFinish()
-            }
-        }
-        timer.start()
-    }
     /**
      * Moves to the next word in the list
      */
