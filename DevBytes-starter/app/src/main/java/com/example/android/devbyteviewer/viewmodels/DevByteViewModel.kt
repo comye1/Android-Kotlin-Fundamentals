1ae5048fe4d66f2f62e941fb6948f1464c58b177
comye1 <50735594+comye1@users.noreply.github.com>
2021-04-14 18:12:40 +0900

    add Room dependencies and database objects

--- DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
+++ DevBytes-starter/app/src/main/java/com/example/android/devbyteviewer/viewmodels/DevByteViewModel.kt
@@ -103,6 +103,7 @@ class DevByteViewModel(application: Application) : AndroidViewModel(application)
             _isNetworkErrorShown.value = false
 
         } catch (networkError: IOException) {
+            // delay(2000) //set a network error to see a loading spinner
             // Show a Toast error message and hide the progress bar.
             _eventNetworkError.value = true
         }
